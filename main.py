5c3c5f7f378cee67ec8964dd8d1fc7f8b7a35c36
Stina <christinama92@gmail.com>
2022-06-16 19:11:37 +0100

    print in new file

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import requests
 import random
 
-pokemon_id = random.choice(range(1, 152))
-response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}")
-pokemon_data = response.json()
+pokemon_ids = random.sample(range(1, 152), 6)
 
-print(f"You got pokemon number {pokemon_data['id']}, {pokemon_data['name'].capitalize()}!")
-print(f"Height: {pokemon_data['height']} | weight: {pokemon_data['weight']}")
+with open('pokemon_encyclopedia.txt', 'w') as pokemon_file:
+    for pokemon_id in pokemon_ids:
+        response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}")
+        # Converts from JavaScript Object Notation into Python lists and dictionaries
+        pokemon_data = response.json()
 
-pokemon_types = []
+        print(f"You got pokemon number {pokemon_data['id']}, {pokemon_data['name'].capitalize()}!", file=pokemon_file)
+        print(f"Height: {pokemon_data['height']} • Weight: {pokemon_data['weight']}", file=pokemon_file)
 
-for type_data in pokemon_data['types']:
-    pokemon_types.append(type_data['type']['name'])
+        pokemon_types = []
+        for type_data in pokemon_data['types']:
+            pokemon_types.append(type_data['type']['name'])
 
-print(f"Types: {', '.join(pokemon_types)}")
+        print(f"Type(s): {' and '.join(pokemon_types)}", file=pokemon_file)
 
-pokemon_abilities = []
+        pokemon_abilities = []
+        for ability_data in pokemon_data['abilities']:
+            pokemon_abilities.append(ability_data['ability']['name'])
 
-for ability_data in pokemon_data['abilities']:
-    pokemon_abilities.append(ability_data['ability']['name'])
-
-print(f"Abilities: {', '.join(pokemon_abilities)}")
+        print(f"Abilities: {', '.join(pokemon_abilities)}", file=pokemon_file)
+        print(file=pokemon_file)
